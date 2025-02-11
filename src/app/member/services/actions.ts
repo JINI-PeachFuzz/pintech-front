cca706d5cd6104d0e1e7785c7d5b276704a43b7f
JINJU PARK <aricclunj@naver.com>
2025-02-11 22:12:14 +0900

    .

--- src/app/member/services/actions.ts
+++ src/app/member/services/actions.ts
@@ -1,5 +1,5 @@
 'use server'
-import { redirect } from 'next/navigation'
+import { redirect, RedirectType } from 'next/navigation'
 import { format } from 'date-fns'
 import { cookies } from 'next/headers'
 import apiRequest from '@/app/global/libs/apiRequest'
@@ -13,22 +13,22 @@ import { revalidatePath } from 'next/cache'
 export const processJoin = async (params, formData: FormData) => {
   const redirectUrl = params?.redirectUrl ?? '/member/login'
 
-  const form = {}
-  let errors = {}
+  const form: any = {}
+  let errors: any = {}
   let hasErrors = false
 
-  for (let [key, value] of formData.entries()) {
+  for (const [key, value] of formData.entries()) {
     if (key.includes('$ACTION')) continue
-
-    if (key === 'birthDt' && value && value.trim()) {
-      value = format(new Date(value), 'yyyy-MM-dd')
+    let _value: string | boolean = value.toString()
+    if (key === 'birthDt' && _value && _value.trim()) {
+      _value = format(new Date(_value), 'yyyy-MM-dd')
     }
 
-    if (['false', 'true'].includes(value)) {
-      value = value === 'true'
+    if (['false', 'true'].includes(_value)) {
+      _value = _value === 'true'
     }
 
-    form[key] = value
+    form[key] = _value
   }
 
   // 필수 항목 검증 S
@@ -57,6 +57,7 @@ export const processJoin = async (params, formData: FormData) => {
   }
 
   // 주소 항목 검증
+
   if (
     !form.zipCode ||
     !form.zipCode?.trim() ||
@@ -114,12 +115,12 @@ export const processJoin = async (params, formData: FormData) => {
 export const processLogin = async (params, formData: FormData) => {
   const redirectUrl = params?.redirectUrl ?? '/'
 
-  let errors = {}
+  let errors: any = {}
   let hasErrors = false
 
   // 필수 항목 검증 S
-  const email = formData.get('email')
-  const password = formData.get('password')
+  const email = formData.get('email').toString()
+  const password = formData.get('password').toString()
   if (!email || !email.trim()) {
     errors.email = errors.email ?? []
     errors.email.push('이메일을 입력하세요.')
@@ -175,7 +176,7 @@ export const processLogin = async (params, formData: FormData) => {
   revalidatePath('/', 'layout')
 
   // 로그인 성공시 이동
-  redirect(redirectUrl)
+  redirect(redirectUrl, RedirectType.replace)
 }
 
 /**
@@ -192,5 +193,7 @@ export const getUserInfo = async () => {
       const result = await res.json()
       return result.success && result.data
     }
-  } catch (err) {}
+  } catch (err) {
+    console.error(err)
+  }
 }
